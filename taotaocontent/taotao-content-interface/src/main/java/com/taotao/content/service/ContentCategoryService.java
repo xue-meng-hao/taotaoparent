37fada794a724b58029430bdae8babe0fdd87179
Xuemenghao <xmhzwy@163.com>
2019-05-07 15:13:02 +0800

    完成后台管理页面分类操作

--- taotaocontent/taotao-content-interface/src/main/java/com/taotao/content/service/ContentCategoryService.java
+++ taotaocontent/taotao-content-interface/src/main/java/com/taotao/content/service/ContentCategoryService.java
@@ -1,10 +1,17 @@
 package com.taotao.content.service;
 
 import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.common.pojo.TaotaoResult;
 
 import java.util.List;
 
 public interface ContentCategoryService {
     //通过节点的id，查询节点的子节点
     public List<EasyUITreeNode> getContentCategoryList(Long parentId);
+
+    //添加内容分类
+    public TaotaoResult createTaotaoResault(Long parentId, String name);
+
+    //根据id删除节点
+    public boolean deleteTaotaoResault(Long parentId);
 }
--- taotaocontent/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentCategoryServiceImpl.java
+++ taotaocontent/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentCategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.common.pojo.EasyUITreeNode;
+import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentCategoryService;
 import com.taotao.mapper.TbContentCategoryMapper;
 import com.taotao.pojo.TbContentCategory;
@@ -8,8 +9,10 @@ import com.taotao.pojo.TbContentCategoryExample;
 import com.taotao.pojo.TbItemExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -43,4 +46,42 @@ public class ContentCategoryServiceImpl implements ContentCategoryService {
         }
         return nodes;
     }
+
+    @Override
+    public TaotaoResult createTaotaoResault(Long parentId, String name) {
+        //1.创建对象 补全属性
+        //2.插入表种的数据
+        //3.返回包含的内容id       需要逐渐返回
+        TbContentCategory tbContentCategory = new TbContentCategory();
+        tbContentCategory.setCreated(new Date());
+        tbContentCategory.setParentId(parentId);
+        //获取父结点
+        TbContentCategory parentCategory = mapper.selectByPrimaryKey(parentId);
+        if (!parentCategory.getIsParent()) {
+            parentCategory.setIsParent(true);
+            mapper.updateByPrimaryKey(parentCategory);
+        }
+        tbContentCategory.setIsParent(false);
+        tbContentCategory.setName(name);
+        tbContentCategory.setSortOrder(1);
+        tbContentCategory.setStatus(1);
+        tbContentCategory.setUpdated(tbContentCategory.getCreated());
+
+        mapper.insert(tbContentCategory);
+
+        return TaotaoResult.ok();
+    }
+
+    @Override
+    public boolean deleteTaotaoResault(Long id) {
+        TbContentCategory tbContentCategory = mapper.selectByPrimaryKey(id);
+        if (!tbContentCategory.getIsParent()) {
+            int i = mapper.deleteByPrimaryKey(id);
+            if (i > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
